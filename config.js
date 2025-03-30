fb541a270da5b8f41ceda3b13cf1b5c54d8f00eb
shubhamos-ai <shubhamos.devilkings@gmail.com>
2025-03-30 17:42:42 +0530

    Update config.js

--- config.js
+++ config.js
@@ -25,10 +25,10 @@ module.exports = {
 	// Lavalink server; public lavalink -> https://lavalink-list.darrennathanael.com/; create one yourself -> https://darrennathanael.com/post/how-to-lavalink
 	nodes: [
 		{
-			identifier: "Jirayu", //- Used for indentifier in stats commands.
-			host: "lavalink.jirayu.net", //- The host name or IP of the lavalink server.
-			port: 13592, // The port that lavalink is listening to. This must be a number!
-			password: "youshallnotpass", //- The password of the lavalink server.
+			identifier: "Amane", //- Used for indentifier in stats commands.
+			host: "lava-all.ajieblogs.eu.org", //- The host name or IP of the lavalink server.
+			port: 80, // The port that lavalink is listening to. This must be a number!
+			password: "https://dsc.gg/ajidevserver", //- The password of the lavalink server.
 			retryAmount: 200, //- The amount of times to retry connecting to the node if connection got dropped.
 			retryDelay: 40, //- Delay between reconnect attempts if connection is lost.
 			secure: false, //- Can be either true or false. Only use true if ssl is enabled!
